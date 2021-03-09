91305e13ff7b0a76e9d9be9ed4c6c067f4064e0f
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2021-03-09 21:56:55 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,12 @@ const Header = () => {
     return (
         <div className="header_wraper">
             <div className="main_info">
-                <h4>Hello,my name is</h4>
+                <h4>Hello, my name is</h4>
                 <h1>Kanchan kumar</h1>
                 <h3>And I'm a 
                 <Typed
                     className="Type-text"
-                    strings={[" Web designer", " Web developer"," Android developer"]}
+                    strings={[" Web designer", " Web developer"," Android developer","php"]}
                     typeSpeed={40}
                     backSpeed={60}
                     loop
